94813cae6810d30770790918f0c3279801a05c94
happai079 <gksyddl22@gmail.com>
2022-08-18 21:35:41 +0900

    Signin 페이지 UI

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import { darkTheme, lightTheme } from './utils/Theme';
 import Home from './pages/Home';
 import Video from './pages/Video';
+import SignIn from './pages/SignIn';
 
 const Container = styled.div`
 	display: flex;
@@ -34,6 +35,7 @@ function App() {
 							<Routes>
 								<Route path="/">
 									<Route index element={<Home />} />
+									<Route path="signin" element={<SignIn />} />
 									<Route path="video">
 										<Route path=":id" element={<Video />} />
 									</Route>
