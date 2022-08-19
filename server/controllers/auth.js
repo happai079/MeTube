58cfb46cd639fb7cc1e118d49382862be8929248
happai079 <gksyddl22@gmail.com>
2022-08-19 14:44:01 +0900

    sign in 로직 - JWT

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
+import { createError } from './../error.js';
+import jwt from 'jsonwebtoken';
 
+// 회원가입
 export const signup = async (req, res, next) => {
 	try {
 		const salt = bcrypt.genSaltSync(10);
@@ -11,6 +14,31 @@ export const signup = async (req, res, next) => {
 		await newUser.save();
 		res.status(200).send('User has been created!');
 	} catch (error) {
-		next(error);
+		next(err);
+	}
+};
+
+// 로그인
+export const signin = async (req, res, next) => {
+	try {
+		const user = await User.findOne({ name: req.body.name });
+		if (!user) return next(createError(404, 'User not found!'));
+
+		const isCorrect = await bcrypt.compare(req.body.password, user.password);
+		if (!isCorrect) return next(createError(400, 'Wraon Credentials!'));
+
+		// 로그인 성공시 JWT 생성 및 cookie에 전달
+		const token = jwt.sign({ id: user._id }, process.env.JWT);
+
+		// rest 연산를 통해서 password는 전달 X
+		const { password, ...others } = user._doc;
+		res
+			.cookie('access_token', token, {
+				httpOnly: true,
+			})
+			.status(200)
+			.json(others);
+	} catch (error) {
+		next(err);
 	}
 };
