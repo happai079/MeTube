bca9d4107c0a51d482f842949d53ed46f4677cb1
happai079 <gksyddl22@gmail.com>
2022-08-24 18:30:31 +0900

    video upload - firevase storage 사용

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {useNavigate} from 'react-router-dom'
 import styled from 'styled-components';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import app from './../firebase';
+import axios from 'axios';
 
 const Container =  styled.div`
   width: 100%;
@@ -68,19 +72,113 @@ const Label = styled.label`
 `;
 
 const Upload = ({setOpen}) => {
+  const [image, setImage] = useState(undefined);
+  const [video, setVideo] = useState(undefined);
+  const [imagePerc, setImagePerc] = useState(0);
+  const [videoPerc, setVideoPerc] = useState(0);
+  const [inputs, setInputs] = useState({});
+  const [tags, setTags] = useState([]);
+  
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs(prev => {
+      return {...prev, [e.target.name]: e.target.value};
+    })
+  };
+
+  const handleTags = (e) => {
+    setTags(e.target.value.split(','));
+  };
+
+  const handleUpload = async (e) => {
+    e.preventDefault();
+    const res = await axios.post('/api/videos', {...inputs, tags});
+    setOpen(false);
+    console.log(res.status)
+    res.status === 200 && navigate(`/video/${res.data._id}`); 
+  };
+
+  const uploadFile = (file, urlType) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        urlType === "imgUrl" ? setImagePerc(Math.round(progress)) : setVideoPerc(Math.round(progress));
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+            break;
+        }
+      },
+      (error) => {},
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setInputs((prev) => {
+            return { ...prev, [urlType]: downloadURL };
+          });
+        });
+      }
+    );
+  };
+
+  useEffect(() => {video && uploadFile(video, 'videoUrl')},[video]);
+  useEffect(() => {image && uploadFile(image, 'imgUrl')},[image]);
+
   return (
     <Container>
       <Wrapper>
         <Close onClick={() => setOpen(false)}>X</Close>
         <Title>Upload a New Video</Title>
         <Label>Video:</Label>
-        <Input type='file' accept='video/*' />
-        <Input type='text' placeholder='Title' />
-        <Desc placeholder='Describtion' rows={8} />
-        <Input type='text' placeholder='Separate the tags with commas.' />
+        {videoPerc > 0 ? (
+          `Uploading: ${videoPerc}%`
+        ) : (
+          <Input 
+            type='file' 
+            accept='video/*' 
+            onChange={(e) => setVideo(e.target.files[0])} 
+          />
+        )}
+        <Input 
+          type='text' 
+          placeholder='Title' 
+          name='title' 
+          onChange={handleChange} 
+        />
+        <Desc 
+          placeholder='Describtion' 
+          rows={8} 
+          name='desc' 
+          onChange={handleChange} 
+        />
+        <Input 
+          type='text' 
+          placeholder='Separate the tags with commas.' 
+          onChange={handleTags} 
+        />
         <Label>Image:</Label>
-        <Input type='file' accept='image/*' />
-        <Button>Upload</Button>
+        {imagePerc > 0 ? (
+          `Uploading: ${imagePerc}%`
+        ) : (
+          <Input 
+            type='file' 
+            accept='image/*' 
+            onChange={(e) => setImage(e.target.files[0])} 
+          />
+        )}
+        <Button onClick={handleUpload} >Upload</Button>
       </Wrapper>
     </Container>
   );
